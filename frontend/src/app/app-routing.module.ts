4c25984c85039d044967a0f54f03f14d67a365b1
Thiago Tavares <tvttavares@gmail.com>
2020-09-03 07:25:43 -0400

    Creating product component and adding navigation routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './view/home/home.component';
+import { ProductCrudComponent } from './view/product-crud/product-crud.component'
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  }, {
+    path: "products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
