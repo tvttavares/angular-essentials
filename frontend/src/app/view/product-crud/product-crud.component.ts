45f86d2ae7cb23290b1b677b99f54a0728961467
Thiago Tavares <tvttavares@gmail.com>
2020-09-03 13:30:01 -0400

    Changing title

--- frontend/src/app/view/product-crud/product-crud.component.ts
+++ frontend/src/app/view/product-crud/product-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
@@ -8,7 +9,13 @@ import { Router } from '@angular/router'
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
+  }
 
   ngOnInit(): void {
   }
@@ -16,4 +23,5 @@ export class ProductCrudComponent implements OnInit {
   navigateToProductCreate(): void {
     this.router.navigate(['/products/create'])
   }
+
 }
