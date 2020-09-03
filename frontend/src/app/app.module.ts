fa4d4c237d42314ffa516f3d63f8a46cf6f65c11
Thiago Tavares <tvttavares@gmail.com>
2020-09-03 12:42:54 -0400

    Implementing detele component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
 import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 import { HttpClientModule } from '@angular/common/http'
 
@@ -47,7 +48,8 @@ registerLocaleData(localePT)
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
-    ProductUpdateComponent
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
